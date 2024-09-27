5e3fd9863f883dcec7dcbc565b12e295194e1470
disheiX <114056052+disheiX@users.noreply.github.com>
2024-09-26 22:37:29 -0300

    Added check password function

--- back/encrypt.py
+++ back/encrypt.py
@@ -5,3 +5,7 @@ def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
+
+def check_password(input_password: str, stored_password: bytes) -> bool:
+    # Check if the hashed password is the same
+    return bcrypt.checkpw(input_password.encode('utf-8'), stored_password)
