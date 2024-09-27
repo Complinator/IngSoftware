0dfdfb85758cb76acf6632420a9f0000bf6f2106
disheiX <renzosda@gmail.com>
2024-09-26 22:47:23 -0300

    Database api stuff

--- back/main.py
+++ back/main.py
@@ -1,5 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from database import save_user, user_exists
+from pydantic import BaseModel
+from encrypt import hash_password, check_password
+
+class UserSignup(BaseModel):
+    email: str
+    password: str
 
 # App object
 app = FastAPI()
@@ -18,3 +25,17 @@ app.add_middleware(
 @app.get("/")
 def read_root():
     return {"HI!!!! o/"}
+
+@app.post("/api/signup")
+async def signup(user: UserSignup):
+    # Check if the user already exists
+    if user_exists(user.email):
+        raise HTTPException(status_code=400, detail="User already exists")
+
+    # Hash the user's password
+    hashed_password = hash_password(user.password)
+
+    # Save the user to the database
+    save_user(user.email, hashed_password)
+
+    return {"message": "User created successfully"}
