1f3f986ee00c1d98a9a218d9ad2bfd0bd8a1aac9
disheiX <114056052+disheiX@users.noreply.github.com>
2024-09-26 22:28:31 -0300

    removed fixed salt comment

--- back/encrypt.py
+++ back/encrypt.py
@@ -2,6 +2,6 @@ import bcrypt
 
 def hash_password(password: str) -> bytes:
     # Generate a salt and hash the password
-    salt = bcrypt.gensalt()  # You can also specify the rounds (e.g., bcrypt.gensalt(12))
+    salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
