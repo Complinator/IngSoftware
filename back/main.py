064585189d4a46989ec1fc0d3518f1cbac7a0cb0
Complinator <bssaldivia@gmail.com>
2024-10-09 21:21:43 -0300

    changes

--- back/main.py
+++ back/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi_jwt_auth import AuthJWT
 from fastapi_jwt_auth.exceptions import AuthJWTException
@@ -27,7 +27,7 @@ class UserSignup(BaseModel):
 
 # App object
 app = FastAPI()
-# chatai = chatAI(api_key)
+chatai = chatAI(api_key)
 readpdf = readPDF(getRelative("LuquilloWMS.pdf"))
 
 # Creating/Loading ai
